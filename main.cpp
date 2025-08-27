ef83d38e6fdb2279ab7f9f0eed886d9d53086c55
Theprogrammer2005 <37733805+Theprogrammer2005@users.noreply.github.com>
2025-08-27 22:10:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,56 @@
+/* code made by Mohamed AbdelWahab 
+on 25-Aug-2025
+
+
+*/
+
+/*
+
+
+
+1.1 Definition A linear combination of x1, ..., xn has the form
+ a1x1 +a2x2 +a3x3 +... +anxn
+ where the numbers a1
+ an Rare the combination’s coefficients. A linear
+ equation in the variables x1, ..., xn has the form a1x1 + a2x2 +a3x3 + +
+ anxn = d where d bleongs to R is the constant.
+ An n-tuple (s1 s2
+ sn) Rn is a solution of, or satisfies, that equation
+ if substituting the numbers s1, ..., sn for the variables gives a true statement:
+ a1s1 +a2s2 + +ansn =d. A system of linear equations
+ a11x1 + a12x2 + + a1nxn = d1
+ a21x1 + a22x2 + + a2nxn = d2
+ .
+ .
+ .
+ am1x1 +am2x2 + +amnxn = dm
+ has the solution (s1 s2
+ sn) if that n-tuple is a solution of all of the equations.
+
+
+*/
+
+/*
+ Theorem (Gauss’s Method) If a linear system is changed to another by one of
+ these operations
+ (1) an equation is swapped with another
+ (2) an equation has both sides multiplied by a nonzero constant
+ (3) an equation is replaced by the sum of itself and a multiple of another
+ then the two systems have the same set of solutions
+ */
 #include <iostream>
 #include <vector>
 #include <string>
 #include <sstream>
 #include <cmath>
 
+
 using namespace std;
 
-// Function to parse equation and extract coefficients
-vector<double> parseEquation(const string& equation) {  //parse equation
+// function to parse equation and extract coefficients
+vector<double> parseEquation(const string& equation) {   //input is a string called by ref
+
+
     vector<double> coeffs(4, 0); // a, b, c, constant
     
     string cleaned = equation;
@@ -20,6 +63,7 @@ vector<double> parseEquation(const string& equation) {  //parse equation
     if (cleaned[0] != '-' && cleaned[0] != '+') cleaned = "+" + cleaned;
 
     // Handle coefficients for a, b, c
+  
     if ((pos = cleaned.find('a')) != string::npos) {
         int i = pos - 1;
         string num = "";
@@ -33,7 +77,7 @@ vector<double> parseEquation(const string& equation) {  //parse equation
         else if (num == "-") coeffs[0] = -1.0;
         else coeffs[0] = stod(num);
     }
-
+    //check for b variable
     if ((pos = cleaned.find('b')) != string::npos) {
         int i = pos - 1;
         string num = "";
@@ -61,6 +105,7 @@ vector<double> parseEquation(const string& equation) {  //parse equation
         else if (num == "-") coeffs[2] = -1.0;
         else coeffs[2] = stod(num);
     }
+
 
     // Handle constant term (after =)
     pos = cleaned.find('=');
@@ -110,9 +155,9 @@ vector<double> gaussianElimination(vector<vector<double>>& matrix) {
 int main() {
     // Input equations
     vector<string> equations = {
-        "a+2b+c=2",
-        "a-b-c=0",
-        "a+b=3"
+        "a+b+c=4",
+        "a-b+c=2",
+        "2a+2b+c=7"
     };
 
     // Parse equations into matrix
@@ -136,6 +181,7 @@ int main() {
         cout << "a = " << solution[0] << endl;
         cout << "b = " << solution[1] << endl;
         cout << "c = " << solution[2] << endl;
+    
     }
     catch (const exception& e) {
         cout << "Error: " << e.what() << endl;
